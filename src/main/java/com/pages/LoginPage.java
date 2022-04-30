018aba8d6107bad4e72fbe0b94a2d1f1433417c8
vijay251996 <vijayrathore23hld@gmail.com>
2022-04-30 20:24:20 +0530

    Second commit

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -5,5 +5,6 @@ public class LoginPage {
 	public void loginpages() {
 		System.out.println("This is Login page");
 		System.out.println("This is 2nd Login page");
+		System.out.println("This is 3rd Login page");
 	}
 }
--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -5,5 +5,6 @@ public class LoginTest {
 	{
 		System.out.println("This is login tests");
 		System.out.println("This is 2nd login tests");
+		System.out.println("This is 3rd Login tests");
 	}
 }
